f39f9d0e7b0453d40cca2d80675fe023265b949d
zhaleh <zhalehelmi@gmail.com>
2023-11-02 11:48:12 +0330

    add function decrease

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const increaseButtonEl = document.querySelector(".counter__button-increase");
 const counterValueEl = document.querySelector(".counter__value");
+const decreaseButtonEl = document.querySelector(".counter__button-decrease");
 
+//---------------------- decrease Button ----------------
+decreaseButtonEl.addEventListener("click", function () {
+  // get current value of counter
+  const counterValue = counterValueEl.textContent;
+
+  //convert value to number type
+  const currentValueAsNumber = +counterValue;
+
+  //decrement by 1
+  const newNumber = currentValueAsNumber - 1;
+  //update counter value with new value
+  counterValueEl.textContent = newNumber;
+});
+
+//-------------------- increase Button --------------------
 increaseButtonEl.addEventListener("click", function () {
   //current value of counter
   const counterVal = counterValueEl.textContent;
