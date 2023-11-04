1b9c6e43df4638e9cc5dd1f5093a4b73e498fd92
zhaleh <zhalehelmi@gmail.com>
2023-11-04 12:09:13 +0330

    add classlist and remove

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+const counterEl = document.querySelector(".counter");
 const increaseButtonEl = document.querySelector(".counter__button-increase");
 const counterValueEl = document.querySelector(".counter__value");
 const decreaseButtonEl = document.querySelector(".counter__button-decrease");
 const resetButtonEl = document.querySelector(".counter__rest-button");
+const counterTitle = document.querySelector(".counter__title");
 
 function decrementCounter() {
   // get current value of counter
@@ -30,8 +32,20 @@ function incrementCounter() {
   const currentValueAsNumber = +counterVal;
 
   //increment by 1
-  const newValue = currentValueAsNumber + 1;
+  let newValue = currentValueAsNumber + 1;
 
+  //check if new value is greater than 5
+  if (newValue > 5) {
+    //if it is, force it to be 5 instead
+    newValue = 5;
+    counterEl.classList.add("counter--limit");
+
+    //update counter title to say limit has been reached
+    counterTitle.innerHTML = "Limit! Buy <b>Pro</b> for >5";
+
+    increaseButtonEl.disabled = true;
+    decreaseButtonEl.disabled = true;
+  }
   //get counter element with new value
   counterValueEl.textContent = newValue;
 }
@@ -42,6 +56,16 @@ increaseButtonEl.addEventListener("click", incrementCounter);
 
 resetButtonEl.addEventListener("click", function () {
   counterValueEl.textContent = 0;
+
+  //reset backgroundColor
+  counterEl.classList.remove("counter--limit");
+
+  //reset counter Title
+  counterTitle.textContent = "Fancy Counter";
+
+  //enable increase and decrease button
+  increaseButtonEl.disabled = false;
+  decreaseButtonEl.disabled = false;
 });
 
 // ----- key down ----
